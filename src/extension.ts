79aefc98213d030fc23590f5cf0c6bf28666d614
huangpengfei <784667332@qq.com>
2025-12-11 14:57:05 +0800

    Remove Vue component and page templates, and add enum hover functionality
    - Deleted YAML templates for Vue components and pages to streamline the extension.
    - Introduced a new command for enum hover functionality, allowing users to generate TypeScript enums with comments.
    - Updated the extension to register the new enum hover provider and related commands.

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from "vscode"
-import registerEnumHover from "./commands/hover"
+import registerEnumHover from "./commands/enum-hover"
 import registerCreateComponents from "./commands/create-components"
 
 /**
